8c93e4e115202dd743e5f3b657ba44663c706464
Jhonatan Mota <savaegt@gmail.com>
2018-07-26 16:47:06 -0300

    Usando JPA

--- src/main/java/com/udemy/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursospring/resources/CategoriaResource.java
@@ -1,4 +1,25 @@
 package com.udemy.cursospring.resources;
 
+import com.udemy.cursospring.model.Categoria;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+//        return "REST está funcionando!";
+        return lista;
+    }
 }
